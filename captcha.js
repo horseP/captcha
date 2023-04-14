8485ae28ae6d1067fb41f61d4747107c273f4fc5
Ndu Emmanuel <nduemmanuel4s@gmail.com>
2023-04-14 21:49:33 +0100

    committed

---
+++ captcha.js
@@ -0,0 +1,31 @@
+// var allValue = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z','1','2','3','4','5','6','7','8','9','0']
+
+// var cVal1 = allValue[Math.floor(Math.random()*allValue.length)];
+// var cVal2 = allValue[Math.floor(Math.random()*allValue.length)];
+// var cVal3 = allValue[Math.floor(Math.random()*allValue.length)];
+// var cVal4 = allValue[Math.floor(Math.random()*allValue.length)];
+// var cVal5 = allValue[Math.floor(Math.random()*allValue.length)];
+// var cVal6 = allValue[Math.floor(Math.random()*allValue.length)];
+
+// var cValue = cVal1+cVal2+cVal3+cVal4+cVal5+cVal6;
+// alert(cValue)
+
+// captchaValue.innerHTML = cValue
+
+// // thisvalue = "";
+// // inputCaptcha.addEventListener('change',function(){
+// //     thisvalue = inputCaptcha.value;
+// //     alert(thisvalue)
+// // })
+
+// //  .addEventListener('click',function(){
+// //     if(cValue == thisvalue){
+// //         alert('valid');
+// //         document.registration_form.submit();
+// //     }else if(inputCaptcha.value == ""){
+// //         alert('Invalid captcha');
+// //     }else{
+// //         alert('Invalid captcha'); 
+// //     }
+// // })
+
---
+++ home.js
@@ -0,0 +1,37 @@
+function sendOTP() {
+    const email = document.getElementById('email');
+    const otpverify = document.getElementsByClassName('submitBtn');
+}
+
+
+let otp_val = Math.floor(Math.random()*10000);
+
+let emailbody = 
+    <h2> Your OTP is </h2>&{opt_val};
+
+    Email.send({
+        secureToken : "e3023dda-5a2d-461b-b1f8-1c5277ca0aa3",
+        To : email.value,
+        From : "nduemmanuel48@gmail.com",
+        Subject : "This is my Cyber Security Project 2",
+        Body : emailbody
+    }).then(
+        message => {
+            if (message === "OK"){
+                alert("OTP send to your mail "+email.value);
+
+                otpverify.style.display = "block";
+                const fields_input = getElementsByClassName;
+                const submitBtn = getElementsByClassName;
+
+                submitBtn.addEventListener('click',()=>{
+                    if(fields_input.value == opt_val){
+                        alert("Email address verified...");
+                    }
+                    else{
+                        alert("Invalied OTP");
+                    }
+                })
+            }
+        }
+    )
